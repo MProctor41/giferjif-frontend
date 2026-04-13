eae5fe8328e3f1d2291eda2c48fbe7f06749feb6
GreyGreg15 <ilee7@msudenver.edu>
2026-04-13 13:21:46 -0600

    Pulled Mason's push and added Hotkey function with it, adjusted add.jsx to allow hotkey to run

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Dashboard from "./pages/Dashboard";
+import GlobalHotkey from "./assets/Features/GlobalHotkey";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <>
+      <Dashboard />
+      <GlobalHotkey />
+    </>
+  );
 }
 
 export default App;
